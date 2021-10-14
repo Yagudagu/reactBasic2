4fafff66edc2d196c5017ba45a5c224c6e005911
Jacob Martin <Jacobhmartian@gmail.com>
2021-10-13 20:37:18 -0600

    github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import * as React from "react";
-import { Switch, Route, Redirect, Router } from "react-router-dom";
+//import { Switch, Route, Redirect, Router } from "react-router-dom";
 
 // Components
-import { Home } from "./Components/Home";
+//import { Home } from "./Components/Home";
 
 function App() {
   return (
     <>
       <div>Balls</div>
-      {/* <Switch>
-        <Route exact path="/" component={Home} />
-
-        <Route render={() => <Redirect to="/" />} />
-      </Switch> */}
     </>
   );
 }
