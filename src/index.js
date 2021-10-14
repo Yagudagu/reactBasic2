47760942c1fdfbc8ee5f214560812b81ad0f5c86
Jacob Martin <Jacobhmartian@gmail.com>
2021-10-13 20:26:56 -0600

    this is it baby

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import App from "./App";
 //smoothScrollPolyfill.polyfill();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
